5fda39b300e6402e7f45523a4a2735cf28ac23d2
dimasdaffa <dimas.de2004@gmail.com>
2024-03-22 11:29:31 +0700

    fixing bug

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,22 @@
+//Scaffold Flutter
 import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Scaffold',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const FirstScreen(), // Panggil FirstScreen di sini
+    );
+  }
+}
 
 class FirstScreen extends StatelessWidget {
   const FirstScreen({Key? key}) : super(key: key);
